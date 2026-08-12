40e70c74d85bb16285f61ecbc6ed617e75a8652b
Guilherme Lemos <guiarger@gmail.com>
2026-08-12 00:15:46 -0300

    Order, Instantm ISO8601 -

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,13 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_user")
@@ -22,6 +24,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
@@ -31,6 +36,12 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
@@ -73,6 +84,14 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public void deleteOrder(Order order) {
+        orders.remove(order);
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -97,7 +116,5 @@ public class User implements Serializable {
             return false;
         return true;
     }
-
-    
 
 }
