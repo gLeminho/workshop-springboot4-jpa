5f2f4b6242cf4533a371667c7d5edc69fe817620
Lemos <156124383+gLeminho@users.noreply.github.com>
2026-09-02 22:58:15 +0000

    Payment one-to-one association

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -2,27 +2,21 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
 
-import com.educandoweb.course.entities.entitiesEnum.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
-public class Order implements Serializable {
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -30,30 +24,21 @@ public class Order implements Serializable {
     private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm;ss'Z'", timezone = "GMT")
-    private Instant moment; // nova forma de se usar o Date Instant é melhor
-
-    private Integer orderStatus;
-
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
+    private Instant moment;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "id.order")
-    private Set<OrderItem> items = new HashSet<>();
+    @OneToOne
+    @MapsId
+    private Order order;
 
-    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL) // na classe dependente utilizamos o @MapsId, utilizamos o CascadeType para as duas entidades terem o mesmo ID
-    private Payment payment;
-
-    public Order() {
+    public Payment(){
 
     }
 
-    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+    public Payment(Long id, Instant moment, Order order) {
         this.id = id;
         this.moment = moment;
-        setOrderStatus(orderStatus);
-        this.client = client;
+        this.order = order;
     }
 
     public Long getId() {
@@ -72,34 +57,12 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
-    public OrderStatus getOrderStatus() {
-        return OrderStatus.valueOf(orderStatus);
+    public Order getOrder() {
+        return order;
     }
 
-    public void setOrderStatus(OrderStatus orderStatus) {
-        if (orderStatus != null) {
-            this.orderStatus = orderStatus.getCode();
-        }
-    }
-
-    public User getClient() {
-        return client;
-    }
-
-    public void setClient(User client) {
-        this.client = client;
-    }
-
-    public Set<OrderItem> getItems() {
-        return items;
-    }
-
-    public Payment getPayment() {
-        return payment;
-    }
-
-    public void setPayment(Payment payment) {
-        this.payment = payment;
+    public void setOrder(Order order) {
+        this.order = order;
     }
 
     @Override
@@ -118,7 +81,7 @@ public class Order implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Order other = (Order) obj;
+        Payment other = (Payment) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
@@ -126,5 +89,12 @@ public class Order implements Serializable {
             return false;
         return true;
     }
+
+    
+
+    
+
+
+
 
 }
